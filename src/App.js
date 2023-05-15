408c413c0a0c63149df4bfd276ea61383ba0d626
Emilija Angeloska <angeloskaemilija10@gmail.com>
2023-05-16 01:34:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./app.scss";
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import Register from "./pages/register/Register";
+import Login from "./pages/login/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+  return(
+<div className="App">
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/watch" element={<Watch/>} />
+    <Route path="/register" element={<Register/>} />
+    <Route path="/login" element={<Login/>} />
+  </Routes>
+  </BrowserRouter>
+  </div>
+  )
 }
 
 export default App;
